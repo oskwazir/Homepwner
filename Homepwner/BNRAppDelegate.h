05ddb5b6f009eb53fc65f7a79fa2b89b2fe5734e
Omer Wazir <omer.wazir@gmail.com>
2014-08-25 19:35:27 -0700

    Initial Commit

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Omer Wazir on 8/25/14.
+//  Copyright (c) 2014 Omer Wazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
