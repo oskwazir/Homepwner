0776c5f384a02a39f6ce2ad622baff6a18a51191
Omer Wazir <omer.wazir@gmail.com>
2014-09-05 08:23:20 -0700

    Create items and add to table view

--- swift/Homepwner/Homepwner/ItemsViewController.swift
+++ swift/Homepwner/Homepwner/ItemsViewController.swift
@@ -12,6 +12,10 @@ class ItemsViewController: UITableViewController {
     
     override init(){
         super.init(style: UITableViewStyle.Plain)
+        
+        for _ in 1...5 {
+            ItemStore.sharedStore.createItem()
+        }
     }
     
     override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
@@ -26,5 +30,23 @@ class ItemsViewController: UITableViewController {
         fatalError("NSCoding not supported")
     }
     
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return ItemStore.sharedStore.allItems.count
+    }
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        // create an instance of UITableViewCell with the default appearance
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "UITableViewCell")
+        
+        //Set the text of the cell with the description of the item
+        //that is at the nth index of ItemStore.sharedStore.allitems
+        
+        let items = ItemStore.sharedStore.allItems
+        let item:Item = items[indexPath.row]
+        
+        cell.textLabel.text = item.description
+        
+        return cell
+    }
 }
-
